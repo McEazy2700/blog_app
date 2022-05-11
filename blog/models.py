21980178e94ac9d0e7335b1e0ac82764e3df74a6
Ezekiel Victor <codeepoch@gmail.com>
2022-05-11 05:40:06 +0100

    Added and migrated the required models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from user .models import CustomUser
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    summary = models.TextField(max_length=160)
+    content = models.TextField()
+    slug = models.SlugField()
+    image = models.ImageField(
+        upload_to='uploads/images/blog/', blank=True, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=20)
+    tag_item = models.ManyToManyField(Blog, related_name='tags')
+
+    def __str__(self):
+        return self.tag_name

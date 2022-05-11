536aa2360c33b61b119aee5727c2df199ad602a0
Ezekiel Victor <codeepoch@gmail.com>
2022-05-11 07:27:54 +0100

    Moved the tag's model to the tag app

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from user .models import CustomUser
+from user.models import CustomUser
+from tag.models import Tag
 
 # Create your models here.
 
@@ -12,14 +13,8 @@ class Blog(models.Model):
     image = models.ImageField(
         upload_to='uploads/images/blog/', blank=True, null=True)
     user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    tag_item = models.ManyToManyField(Tag, related_name='tags', null=True)
 
     def __str__(self):
         return self.title
 
-
-class Tag(models.Model):
-    tag_name = models.CharField(max_length=20)
-    tag_item = models.ManyToManyField(Blog, related_name='tags')
-
-    def __str__(self):
-        return self.tag_name

9895be4fb2d60ec93e34efdcb6bc1fabceb42f5f
Ezekiel Victor <codeepoch@gmail.com>
2022-05-12 09:12:32 +0100

    Started afresh

--- tag/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Tag(models.Model):
-    tag_name = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.tag_name
